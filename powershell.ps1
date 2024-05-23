eb8818cd81d1350041b728743ad0cc39e1b605fb
Muhammad Tausif <tausifasia@gmail.com>
2024-05-24 02:52:49 +0500

    Create powershell.ps1 and echo basic
    Created powershell file
    Added few echo commands
    Saving data, appending data.

---
+++ powershell.ps1
@@ -0,0 +1,19 @@
+This file is about powershell commands:
+
+# To enter a text on screen
+echo "First line"
+
+# To save a line to text
+echo "Second line" > file.txt
+
+# To save multiline
+echo "First line
+
+second line
+
+Third line" > file.txt
+
+# To append data to a file
+echo "Appending data
+
+new line data" >> file.txt
